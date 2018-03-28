952ae4193d7c47af0c46528f78227af237510acb
Martin Meyer <martin.meyer@gmx.ch>
2018-03-28 17:06:47 +0200

    ..

--- craft/config/general.php
+++ craft/config/general.php
@@ -1,33 +1,44 @@
 <?php
 
-/**
- * General Configuration
- *
- * All of your system's general configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
- */
-
 return array(
+  '*' => array(
+    'devMode' => false,
+    'generateTransformsBeforePageLoad' => true,
+    'enableCsrfProtection' => false,
+    'omitScriptNameInUrls' => true,
+    'limitAutoSlugsToAscii' => true,
+    'convertFilenamesToAscii' => true,
+    'allowAutoUpdates' => false,
+  ),
 
-	// Base site URL
-	'siteUrl' => null,
 
-	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
-	'environmentVariables' => array(),
+	'buerohaeberli.local' => array(
+	    'devMode' => true,
+	    // 'cacheDuration' => 'PT1S',
+	    'allowAutoUpdates' => true,
 
-	// Default Week Start Day (0 = Sunday, 1 = Monday...)
-	'defaultWeekStartDay' => 0,
+	    'environmentVariables' => array(
+			'basePath' => $_SERVER['DOCUMENT_ROOT'].'/user_assets/',
+			'baseUrl'  => 'http://buerohaeberli.local:8080/',
+	    ),
 
-	// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-	'enableCsrfProtection' => true,
+	    // 'siteUrl' => array(
+	    //   'de' => 'http://localhost:3000/',
+	    //   'en' => 'http://localhost:3000/en/',
+	    // ),
+	),
 
-	// Whether "index.php" should be visible in URLs (true, false, "auto")
-	'omitScriptNameInUrls' => 'auto',
+	'client.ch' => array(
+		'cacheDuration' => '0',
+    	'devMode' => false,
 
-	// Control Panel trigger word
-	'cpTrigger' => 'admin',
+	    'environmentVariables' => array(
+	     	'basePath' => $_SERVER['DOCUMENT_ROOT'].'/user_assets/',
+	    ),
 
-	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => false,
-
+    	// 'siteUrl' => array(
+     	//		'de' => 'https://www.tribecraft.ch/',
+     	//		'en' => 'https://www.tribecraft.ch/en/',
+    	// )
+  	)
 );
